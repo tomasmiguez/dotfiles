6f6337cec384e4dbb20119d45c041782cc1420f9
Tomas Miguez <tomasmiguez99@gmail.com>
2023-08-20 14:25:36 -0300

    chore(xmonad): sort layouts

--- .config/xmonad/lib/MyLayout.hs
+++ .config/xmonad/lib/MyLayout.hs
@@ -7,7 +7,7 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Hooks.ManageDocks
 
-myLayout = avoidStruts $ tiled ||| Mirror tiled ||| noBorders Full
+myLayout = avoidStruts $ noBorders Full ||| tiled ||| Mirror tiled
   where
     tiled   = spacingWithEdge 5 $ Tall nmaster delta ratio
     nmaster = 1      -- Default number of windows in the master pane
