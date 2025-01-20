59ccbf654d15422913ec91d201ca2b2854844128
Tomás Miguez <tmiguez@fu.do>
2025-01-20 14:39:00 -0300

    feat(nvim): configure `luasnips`

---
+++ .config/nvim/lua/tmiguez/config/luasnips.lua
@@ -0,0 +1,18 @@
+require("luasnip").config.set_config {
+  enable_autosnippets = true,
+  -- store_selection_keys = "<Tab>",
+}
+
+vim.cmd [[
+" Expand or jump in insert mode
+imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+
+" Jump forward through tabstops in visual mode
+smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
+
+" Jump backward through snippet tabstops with Shift-Tab
+imap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
+]]
+
+require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/luasnippets/"})
